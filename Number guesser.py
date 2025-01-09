54e2d298cd36a04ff7fa6e027d7d8855a27ca172
Кика <lozkinn097@gmail.com>
2025-01-09 15:22:57 +0300

    second

--- Number guesser.py
+++ Number guesser.py
@@ -1,19 +1,21 @@
 #game - Number guesser
+import random
 def UIrender():
     pass
 
-
-def Main():
-    StartMessage = "Hi, it's a Number guesser game\n"\
+print("Hi, it's a Number guesser game\n"\
           "Try to guess my number!\n"\
-          "it's between 1 and 100\n\n"\
-          "Please select difficulty level:\n"\
+          "it's between 1 and 100\n\n")
+def Main():
+    StartMessage = "Please select difficulty level:\n"\
           "1 - Easy (10 chances)\n"\
           "2 - Medium (5 chances)\n"\
           "3 - Hard (3 chances)\n"\
 
     print(StartMessage)      
     while True:
+        attempts: int = 1 
+        number: int = random.randint(1,100)
         try:
             dificult: int = int(input("your choice: "))
             if dificult not in [1, 2, 3]:  
@@ -24,14 +26,32 @@ def Main():
             continue
         match(dificult):
             case 1:
-                print("\nGreat, You choise Easy level! You have 10 chances")     
-                break           
+                print("\nGreat, You choise Easy level! You have 10 chances") 
+                chances: int = 10    
+                       
             case 2:
                 print("\nGreat, You choise Medium level! You have 5 chances")  
-                break
+                chances: int = 5  
+            
             case 3:
                 print("\nGreat, You choise Hard level! You have only 3 chances") 
-                break 
+                chances: int = 3 
 
+        print("Let's start the game!")
+        for i in range(chances):
+            UserNum = int(input("\nEnter your guess: "))
+            if UserNum == number:
+                print("\nCongratulations! You won\n")
+                return attempts
+            elif UserNum > number:
+                print(f'No, my number is less than {UserNum}. Try again')
+            else:
+                print(f'No, my number is greater than {UserNum}. Try again')
+        return 0
 
-Main()
+while True:
+    result = Main()
+    if result == 0:
+        print("\nYou lose, but it's just a game! Try again!\n")
+    else:
+        print(f"Good job you won with {result} attempts! Try again!" + "\n")
